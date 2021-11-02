b0628bcb1ed2d3bda1eb97871f2ca7f091c5ddf7
YupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2021-11-02 13:54:27 +0500

    add style.css and Fetch API

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   getBitcoinPrice();
 });
-
-document.querySelector("button").addEventListener("click", () => {
-  //1. XMLHttpRequest ni hosil qilish
-  console.log("hi");
-  let xhr1 = new XMLHttpRequest();
-  //2. Event Handler
-  xhr1.onload = function () {
-    let obj = JSON.parse(this.responseText);
-    let inputt = document.querySelector("input");
-    let data = obj[inputt.value.toLowerCase()];
-    let text = document.querySelector("#rate");
-    if (data != undefined) {
-      text.innerHTML = data;
-    } else {
-      text.innerHTML = `Bunday birlik yo'q`;
-    }
-  };
-  // GET request;
-  xhr1.open(
-    "GET",
-    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
-  );
-  //yuborish kere
-  xhr1.send();
-});
+//========================================
+//xmlhttpsrequest usuli bilan edi
+//===============================
+// document.querySelector("button").addEventListener("click", () => {
+//   //1. XMLHttpRequest ni hosil qilish
+//   console.log("hi");
+//   let xhr1 = new XMLHttpRequest();
+//   //2. Event Handler
+//   xhr1.onload = function () {
+//     let obj = JSON.parse(this.responseText);
+//     let inputt = document.querySelector("input");
+//     let data = obj[inputt.value.toLowerCase()];
+//     let text = document.querySelector("#rate");
+//     if (data != undefined) {
+//       text.innerHTML = data;
+//     } else {
+//       text.innerHTML = `Bunday birlik yo'q`;
+//     }
+//   };
+//   // GET request;
+//   xhr1.open(
+//     "GET",
+//     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+//   );
+//   //yuborish kere
+//   xhr1.send();
+// });
 
 function updateBitcoinPrice(newValue) {
   document.querySelector("#narxi").innerHTML = `${newValue} (USD)`;
@@ -51,3 +53,24 @@ function getBitcoinPrice() {
   //4. AJAX requestni web serga yuborish kerak
   xhr.send();
 }
+
+//=========================================
+//           fetch api bilan
+//================================
+document.querySelector("button").addEventListener("click", () => {
+  fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+  )
+    .then((request) => request.json())
+    .then((data) => {
+      let inputt = document.querySelector("input");
+      let text = document.querySelector("#rate");
+      if (data[inputt.value.toLowerCase()] != undefined) {
+        text.innerHTML = data[inputt.value.toLowerCase()];
+        text.style.color = "green";
+      } else {
+        text.innerHTML = `Bunday birlik yo'q`;
+        text.style.color = "red";
+      }
+    });
+});
